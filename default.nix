49374b0eefdbc805b6a6d9521e90650d00f4fb87
zimbatm <zimbatm@zimbatm.com>
2022-02-19 11:47:32 +0100

    add flake checks
    Re-organize the project so running `flake checks` will build all of the
    unfree + redistributable packages.
    
    Move most of the logic in default.nix to make it more easily accessible.

---
+++ default.nix
@@ -0,0 +1,66 @@
+{ system ? builtins.currentSystem
+, nixpkgs ? import <nixpkgs> {
+    inherit system;
+    config = { allowUnfree = true; };
+  }
+, lib ? nixpkgs.lib
+, debug ? false
+}:
+let
+  trace = if debug then builtins.trace else (msg: value: value);
+  # Tweaked version of nixpkgs/maintainers/scripts/check-hydra-by-maintainer.nix
+  #
+  # It traverses nixpkgs recursively, respecting recurseForDerivations and 
+  # returns a list of name/value pairs of all the packages matching "cond"
+  packagesWith = prefix: cond: set:
+    lib.flatten
+      (lib.mapAttrsToList
+        (key: v:
+          let
+            name = "${prefix}${key}";
+            result = builtins.tryEval
+              (
+                if lib.isDerivation v && cond name v then
+                # Skip packages whose closure fails on evaluation.
+                # This happens for pkgs like `python27Packages.djangoql`
+                # that have disabled Python pkgs as dependencies.
+                  builtins.seq v.outPath [ (lib.nameValuePair name v) ]
+                else if v.recurseForDerivations or false || v.recurseForRelease or false
+                # Recurse
+                then packagesWith "${name}_" cond v
+                else [ ]
+              );
+          in
+          if result.success
+          then trace name result.value
+          else [ ]
+        )
+        set
+      )
+  ;
+
+  isUnfreeRedistributable = licenses:
+    lib.lists.any (l: (!l.free or true) && (l.redistributable or false)) licenses;
+
+  hasLicense = pkg:
+    pkg ? meta.license;
+
+  hasUnfreeRedistributableLicense = pkg:
+    hasLicense pkg &&
+    isUnfreeRedistributable (lib.lists.toList pkg.meta.license);
+
+in
+{
+  # Export the whole tree
+  legacyPackages = nixpkgs;
+
+  # Returns the recursive set of unfree but redistributable packages as checks
+  checks =
+    lib.listToAttrs (
+      packagesWith
+        ""
+        (name: pkg: hasUnfreeRedistributableLicense pkg)
+        nixpkgs
+    )
+  ;
+}
--- flake.nix
+++ flake.nix
@@ -4,18 +4,34 @@
 
   outputs = { self, nixpkgs }:
     let
+      # Only support systems for which we have a CI for.
+      systems = [ "x86_64-linux" ];
+
       lib = nixpkgs.lib;
-    in
-    # Get the instance of nixpkgs
-    nixpkgs // {
-      # But replace legacyPackages with the unfree version
-      legacyPackages = lib.genAttrs lib.systems.supported.hydra (system:
-        import nixpkgs {
-          inherit system;
-          config = {
-            allowUnfree = true;
+
+      eachSystem = lib.genAttrs systems;
+
+      x = eachSystem (system:
+        import ./. {
+          nixpkgs = import nixpkgs {
+            inherit system;
+            config = { allowUnfree = true; };
           };
+          lib = nixpkgs.lib;
+          # Should not be needed
+          system = null;
         }
       );
+
+    in
+    {
+      # Inherit from upstream
+      inherit (nixpkgs) lib nixosModules htmlDocs;
+
+      # But replace legacyPackages with the unfree version
+      legacyPackages = eachSystem (system: x.${system}.legacyPackages);
+
+      # And load all the unfree+redistributable packages as checks
+      checks = eachSystem (system: x.${system}.checks);
     };
 }
