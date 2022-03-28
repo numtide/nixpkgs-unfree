f2f4e9d031923ed1792c15c638101b3cbbe533ca
Someone Serge <sergei.kozlukov@aalto.fi>
2022-03-28 17:39:10 +0300

    configs: try to account for different cudnn names
    ...in different releases

--- configs.nix
+++ configs.nix
@@ -28,9 +28,16 @@ in
 
   mklCuda11 =
     let
+      potentialNames = {
+        cudnn = [ "cudnn_cudatoolkit_11" "cudnn_8_3_cudatoolkit_11" ];
+      };
+
+      tryByName = pkgs: name: if builtins.hasAttr name pkgs then [ pkgs.${name} ] else [ ];
+      byAnyName = pkgs: name: builtins.head (pkgs.lib.concatMap (tryByName pkgs) potentialNames.${name});
+
       overlay = final: prev: {
         cudatoolkit = final.cudatoolkit_11;
-        cudnn = final.cudnn_cudatoolkit_11;
+        cudnn = byAnyName final "cudnn";
         cutensor = final.cutensor_cudatoolkit_11;
 
         mpich = prev.mpich.override {
