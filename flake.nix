6fdd877dde2d6a6cfc17879fcf87c7e9b35f1397
zimbatm <zimbatm@zimbatm.com>
2022-12-06 20:54:39 +0100

    prevent importing the root
    Reduce confusion when code is creating a new instance of nixpkgs.

--- flake.nix
+++ flake.nix
@@ -14,25 +14,102 @@
       lib = nixpkgs.lib;
 
       eachSystem = lib.genAttrs systems;
-
-      x = eachSystem (system:
-        import ./. {
-          inherit system inputs;
-          lib = nixpkgs.lib;
-        }
-      );
     in
     {
       # Inherit from upstream
       inherit (nixpkgs) lib nixosModules htmlDocs;
 
+      # Expose our own unfree overrides
+      overlays.default = import ./overlay.nix;
+
       # But replace legacyPackages with the unfree version
-      legacyPackages = eachSystem (system: x.${system}.legacyPackages);
+      legacyPackages = eachSystem (system:
+        import nixpkgs {
+          inherit system;
+          config = {
+            allowUnfree = true;
+            cudaSupport = true;
+          };
+          overlays = [ self.overlays.default ];
+        }
+      );
 
       # And load all the unfree+redistributable packages as checks
-      checks = eachSystem (system: x.${system}.checks);
+      checks = eachSystem
+        (system:
+          let
+            pkgs = self.legacyPackages.${system};
 
-      # Expose our own unfree overrides
-      overlay = ./overlay.nix;
+            # Turn this on to debug things.
+            debug = false;
+
+            trace = if debug then builtins.trace else (msg: value: value);
+            # Tweaked version of nixpkgs/maintainers/scripts/check-hydra-by-maintainer.nix
+            #
+            # It traverses nixpkgs recursively, respecting recurseForDerivations and 
+            # returns a list of name/value pairs of all the packages matching "cond"
+            packagesWith = prefix: cond: set:
+              lib.flatten
+                (lib.mapAttrsToList
+                  (key: v:
+                    let
+                      name = "${prefix}${key}";
+                      result = builtins.tryEval
+                        (
+                          if lib.isDerivation v && cond name v then
+                          # Skip packages whose closure fails on evaluation.
+                          # This happens for pkgs like `python27Packages.djangoql`
+                          # that have disabled Python pkgs as dependencies.
+                            builtins.seq v.outPath [ (lib.nameValuePair name v) ]
+                          else if v.recurseForDerivations or false || v.recurseForRelease or false
+                          # Recurse
+                          then packagesWith "${name}_" cond v
+                          else [ ]
+                        );
+                    in
+                    if result.success
+                    then trace name result.value
+                    else [ ]
+                  )
+                  set
+                )
+            ;
+
+            isUnfreeRedistributable = licenses:
+              lib.lists.any (l: (!l.free or true) && (l.redistributable or false)) licenses;
+
+            hasLicense = pkg:
+              pkg ? meta.license;
+
+            hasUnfreeRedistributableLicense = pkg:
+              hasLicense pkg &&
+              isUnfreeRedistributable (lib.lists.toList pkg.meta.license);
+
+            unfreeRedistributablePackages = packagesWith
+              ""
+              (name: pkg: hasUnfreeRedistributableLicense pkg)
+              nixpkgs;
+
+            /* Return an attribute from a nested attribute set.
+
+              Example:
+              x = {a = { b = 3; }; }
+              getAttr "a.b" x
+              => 3
+              getAttr "a.floo" x
+            */
+            getAttr = attrPath:
+              let
+                attrPath_ = lib.filter lib.isString (builtins.split "\\\." attrPath);
+              in
+              lib.attrByPath attrPath_ (abort "cannot find attribute '" + attrPath "'");
+
+            extraChecks = map
+              (name: lib.nameValuePair (lib.replaceStrings [ "." ] [ "_" ] name) (getAttr name pkgs))
+              pkgs.extraChecks;
+          in
+          # Returns the recursive set of unfree but redistributable packages as checks
+          lib.listToAttrs (unfreeRedistributablePackages ++ extraChecks)
+        );
     };
 }
