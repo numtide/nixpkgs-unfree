825769e4d1fdc2433474426d5e55c2541c713e0b
zimbatm <zimbatm@zimbatm.com>
2023-01-13 17:01:31 +0100

    ci: output markdown report

--- ci.sh
+++ ci.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
 args=(
   "$@"
   --accept-flake-config
@@ -11,25 +12,45 @@ args=(
   ./ci.nix
 )
 
+if [[ -n "$GITHUB_STEP_SUMMARY" ]]; then
+  log() {
+    echo "$*" >> "$GITHUB_STEP_SUMMARY"
+  }
+else
+  log() {
+    echo "$*"
+  }
+fi
+
 export NIXPKGS_ALLOW_UNSUPPORTED_SYSTEM=1
 error=0
 
 for job in $(nix run github:nix-community/nix-eval-jobs -- "${args[@]}" | jq -r '. | @base64'); do
-  echo "=========================="
   job=$(echo "$job" | base64 -d)
+  attr=$(echo "$job" | jq -r .attr)
+  echo "### $attr"
   error=$(echo "$job" | jq -r .error)
-  if [[ -z $error ]]; then
-    echo "ERROR: $error"
+  if [[ $error != null ]]; then
+    log "### ❌ $attr"
+    log
+    log "<details><summary>Eval error:</summary><pre>"
+    log "$error"
+    log "</pre></details>"
     error=1
   else
-    echo "$job"
     drvPath=$(echo "$job" | jq -r .drvPath)
-    set +e
-    nix-store --realize "$drvPath"
-    set -e
-    if ! nix-store --realize "$drvPath"; then
+    if ! nix-store --realize "$drvPath" 2>&1 | tee build-log.txt; then
+      log "### ❌ $attr"
+      log
+      log "<details><summary>Build error:</summary>last 50 lines:<pre>"
+      log "$(tail -n 50 build-log.txt)"
+      log "</pre></details>"
       error=1
+    else
+      log "### ✅ $attr"
     fi
+    log
+    rm build-log.txt
   fi
 done
 
